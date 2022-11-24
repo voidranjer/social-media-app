dfab429c7d5e2a66e1b6458ce676d24ca0e11e7b
meekranjer <logicism81@gmail.com>
2022-11-23 22:32:09 -0500

    Avatar update

--- src/components/profile/Avatar.js
+++ src/components/profile/Avatar.js
@@ -2,14 +2,14 @@ import { Avatar as ChakraAvatar } from "@chakra-ui/react";
 import { PROTECTED } from "lib/routes";
 import { Link } from "react-router-dom";
 
-export default function Avatar({ user, size = "xl" }) {
+export default function Avatar({ user, size = "xl", overrideAvatar = null }) {
   return (
     <ChakraAvatar
       as={Link}
       to={`${PROTECTED}/profile/${user.id}`}
       name={user.username}
       size={size}
-      src={user.avatar}
+      src={overrideAvatar || user.avatar}
       _hover={{ cursor: "pointer", opacity: "0.8" }}
     />
   );
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,15 +1,27 @@
-import { Divider, Flex, HStack, Stack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Divider,
+  Flex,
+  HStack,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import PostsList from "components/post/PostsList";
 import { usePosts } from "hooks/posts";
 import { useUser } from "hooks/users";
 import { useParams } from "react-router-dom";
 import Avatar from "./Avatar";
 import { format } from "date-fns";
+import EditProfile from "./EditProfile";
+import { useAuth } from "hooks/auth";
 
 export default function Profile() {
   const { id } = useParams();
   const { posts, isLoading: postsLoading } = usePosts(id);
   const { user, isLoading: userLoading } = useUser(id);
+  const { user: authUser, isLoading: authLoading } = useAuth();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   if (userLoading) return "Loading...";
 
@@ -17,6 +29,20 @@ export default function Profile() {
     <Stack spacing="5">
       <Flex p={["4", "6"]} pos="relative" align="center">
         <Avatar size="2xl" user={user} />
+
+        {!authLoading && authUser.id === user.id && (
+          <Button
+            pos="absolute"
+            mb="2"
+            top="6"
+            right="6"
+            colorScheme="teal"
+            onClick={onOpen}
+          >
+            Change avatar
+          </Button>
+        )}
+
         <Stack ml="10">
           <Text fontSize="2xl">{user.username}</Text>
           <HStack spacing="10">
@@ -31,6 +57,8 @@ export default function Profile() {
             </Text>
           </HStack>
         </Stack>
+
+        <EditProfile isOpen={isOpen} onClose={onClose} />
       </Flex>
       <Divider />
 
