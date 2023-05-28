7c2dc6410aa9eeb0166d5b4275eadf6203d14d90
Robert Bennet <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-05-28 17:20:28 +0000

    Update index.js

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,21 +1,63 @@
 import { Box, Text } from "@chakra-ui/react";
 import Header from "./Header";
 import Actions from "./Actions";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Divider,
+} from "@chakra-ui/react";
+
+function VerticallyCenter({ post }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { title, text } = post;
+
+  return (
+    <>
+      <Button onClick={onOpen}>View More</Button>
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{text}</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 export default function Post({ post }) {
-  const { text } = post;
+  const { title, text } = post;
 
   return (
     <Box p="2" maxW="600px" textAlign="left">
       <Box border="2px solid" borderColor="gray.100" borderRadius="md">
         <Header post={post} />
-
-        <Box p="2" minH="100px">
-          <Text wordBreak="break-word" fontSize="md">
-            {text}
+        <Box p="2">
+          <Text wordBreak="break-word" fontSize="md" as="b">
+            Project Title:
           </Text>
+          {title}
+          <Divider />
+          <Text wordBreak="break-word" fontSize="md" as="b">
+            Project Description:
+          </Text>
+          {text}
         </Box>
-
+        <Box p="2">
+          <VerticallyCenter post={post}></VerticallyCenter>
+        </Box>
         <Actions post={post} />
       </Box>
     </Box>
